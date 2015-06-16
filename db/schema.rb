5e9de749ae19997f96a8904fa17660e6a55c706a
Sia <siakaramalegos@gmail.com>
2015-06-16 10:45:07 -0500

    Migrated users

--- db/schema.rb
+++ db/schema.rb
@@ -37,11 +37,9 @@ ActiveRecord::Schema.define(version: 20150616152526) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.integer  "recipes_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["recipes_id"], name: "index_users_on_recipes_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
