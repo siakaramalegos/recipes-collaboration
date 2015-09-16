8f00cd5cf6d8d5478502c12d85edd1edfa4c314b
evorick <rickkadlac@gmail.com>
2015-09-16 09:11:50 -0500

    removed page from whitelisted params

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -82,6 +82,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:title, :ingredients, :steps, :user_id, :page)
+      params.require(:recipe).permit(:title, :ingredients, :steps, :user_id)
     end
 end
