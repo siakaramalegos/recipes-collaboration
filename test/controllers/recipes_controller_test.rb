dddee7f6c8d59fee702a4d8413b90a52e4e727f3
Sia <siakaramalegos@gmail.com>
2015-12-08 11:27:56 -0600

    Cleans up tests for user login

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -11,12 +11,14 @@ class RecipesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:recipes)
   end
 
-  test "should get new" do
+  test "logged in should get new" do
+    sign_in users(:one)
     get :new
     assert_response :success
   end
 
-  test "should create recipe" do
+  test "logged in should create recipe" do
+    sign_in users(:one)
     assert_difference('Recipe.count') do
       post :create, recipe: { ingredients: @recipe.ingredients, steps: @recipe.steps, title: @recipe.title }
     end
@@ -29,17 +31,20 @@ class RecipesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "logged in should get edit" do
+    sign_in users(:one)
     get :edit, id: @recipe
     assert_response :success
   end
 
-  test "should update recipe" do
+  test "logged in should update recipe" do
+    sign_in users(:one)
     patch :update, id: @recipe, recipe: { ingredients: @recipe.ingredients, steps: @recipe.steps, title: @recipe.title }
     assert_redirected_to recipe_path(assigns(:recipe))
   end
 
-  test "should destroy recipe" do
+  test "logged in should destroy recipe" do
+    sign_in users(:one)
     assert_difference('Recipe.count', -1) do
       delete :destroy, id: @recipe
     end
