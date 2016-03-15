dfb914275de3d4bf2b600cfa5373f8bfc0e46dfa
girllogic <lillianlovich@yahoo.com>
2016-03-15 16:34:46 -0500

    deleted some code used for debugging as it was not necessary/not the problem

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,8 +2,6 @@ class Recipe < ActiveRecord::Base
 	belongs_to :users
 
   has_attached_file :photo,
-    :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
-    :url => "/system/:attachment/:id/:style/:filename",
     :styles => { large: "580x300#", medium: "300x200#", thumb: "100x100#" },
     default_url: ":style_missing.png"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
